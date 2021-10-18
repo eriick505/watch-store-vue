9381fe8ffb29a085ee478ac9fcbccbd9bcf11d2d
Erick <santoserick9@gmail.com>
2021-10-18 09:16:23 -0300

    Cart Manager complete

--- managers/CartManager.unit.spec.js
+++ managers/CartManager.unit.spec.js
@@ -14,6 +14,19 @@ describe('CartManager', () => {
     server.shutdown();
   });
 
+  it('should return the state', () => {
+    const product = server.create('product');
+    manager.addProduct(product);
+    manager.open();
+
+    const state = manager.getState();
+
+    expect(state).toEqual({
+      items: [product],
+      open: true,
+    });
+  });
+
   it('should set cart to open', () => {
     const state = manager.open();
 
@@ -29,19 +42,59 @@ describe('CartManager', () => {
   it('should add product to the cart only once', () => {
     const product = server.create('product');
     manager.addProduct(product);
-
     const state = manager.addProduct(product);
 
     expect(state.items).toHaveLength(1);
   });
 
-  it.todo('should remove product from the cart');
+  it('should remove product from the cart', () => {
+    const product = server.create('product');
 
-  it.todo('should clear products');
+    const state = manager.removeProduct(product.id);
 
-  it.todo('should clear cart');
+    expect(state.items).toHaveLength(0);
+  });
 
-  it.todo('should return true if cart is not empty');
+  it('should clear products', () => {
+    const product1 = server.create('product');
+    const product2 = server.create('product');
 
-  it.todo('should return true if product is already in the cart');
+    manager.addProduct(product1);
+    manager.addProduct(product2);
+
+    const state = manager.clearProducts();
+
+    expect(state.items).toHaveLength(0);
+  });
+
+  it('should clear cart', () => {
+    const product1 = server.create('product');
+    const product2 = server.create('product');
+
+    manager.addProduct(product1);
+    manager.addProduct(product2);
+    manager.open();
+
+    const state = manager.clearCart();
+
+    expect(state.items).toHaveLength(0);
+    expect(state.open).toBe(false);
+  });
+
+  it('should return true if cart is not empty', () => {
+    const product1 = server.create('product');
+    const product2 = server.create('product');
+
+    manager.addProduct(product1);
+    manager.addProduct(product2);
+
+    expect(manager.hasProducts()).toBe(true);
+  });
+
+  it('should return true if product is already in the cart', () => {
+    const product = server.create('product');
+    manager.addProduct(product);
+
+    expect(manager.productIsInTheCart(product)).toBe(true);
+  });
 });
